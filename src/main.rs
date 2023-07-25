9f35b4709cde5217e8c27c93fdd4e6c1faf602f5
Mike Krüger <mkrueger@posteo.de>
2023-07-25 08:15:45 +0200

    Added linux bundle option.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod ui;
 
 fn main() {
     eframe::run_native(
-        "iCY View",
+        &format!("iCY VIEW {}", env!("CARGO_PKG_VERSION")),
         eframe::NativeOptions::default(),
         Box::new(|cc| {
             let mut fd = ui::MainWindow::new(cc, None);
